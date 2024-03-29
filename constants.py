1b27b68573898509b7b9d074030ffb6c6d4161dc
Kevin Kinsey <kevinkinsey2005@gmail.com>
2024-03-29 19:47:47 +0530

    Setting up grouped constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+class Physics:
+    g = 9.8
+
+class UnitScale:
+    kWh = 1000*3600
+
+class CarData:
+    mass = 280  # kgs
+    aero_drag = 0.12 * 1  # SI
+    fric_coeff = 0.045  # SI
+
+    max_battery_capacity = 0.0001 * UnitScale.kWh
+
+    motor_efficiency = 0.98
+
+    max_acceleration = 1  # m/s^2
+    max_velocity = 30  # m/s
+
+    panel_area = 4  # m^2
