df2354ef9fb204bd6145f6dd326c82c8b3e91d73
Kevin Kinsey <kevinkinsey2005@gmail.com>
2024-03-29 21:18:33 +0530

    Add constraint equations

---
+++ constraints.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from config import STRICT_PENALTY
+from constants import CarData
+
+def battery(battery_profile):
+    # m = tf.reduce_min(battery_profile)
+    # const1 = tf.maximum(0.0, STRICT_PENALTY * -m)
+    # const2 = (m)**-10 if m >= 0 else 0.0
+    # return const1 + const2
+    return tf.maximum(0.0, STRICT_PENALTY * -tf.reduce_min(battery_profile))
+
+def velocity(velocity_profile):
+    upper_lim = tf.maximum(0.0, STRICT_PENALTY*(tf.reduce_max(velocity_profile - CarData.max_velocity)))
+    lower_lim = tf.maximum(0.0, -STRICT_PENALTY*(tf.reduce_min(velocity_profile)))
+    return upper_lim + lower_lim
+
+def acceleration(acceleration_profile):
+    return tf.maximum(0.0, STRICT_PENALTY*tf.reduce_max(tf.abs(acceleration_profile) - CarData.max_acceleration))
